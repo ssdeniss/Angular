109ff68c5a7115fbafbaad17dfc4e37c727ebb31
ssdeniss <denis.seremet@gmail.com>
2023-09-12 16:41:18 +0300

    first

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { IProduct } from './models/product';
+import { products as data } from './data/products';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'angular';
+
+  products: IProduct[] = data;
+}
